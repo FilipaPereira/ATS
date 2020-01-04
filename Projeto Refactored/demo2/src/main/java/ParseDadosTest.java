dea6b1b24662115b22153df03a1b8b0815c94ae5
CarlaCruz146 <carla.cruz.247@gmail.com>
2020-01-04 16:28:16 +0000

    testes

---
+++ Projeto Refactored/demo2/src/main/java/ParseDadosTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParseDadosTest {
+
+    @Test
+    public void parseProprietario() {
+    }
+
+    @Test
+    public void parseCliente() {
+    }
+
+    @Test
+    public void parseVeiculo() {
+    }
+}
---
+++ Projeto Refactored/demo2/src/main/java/UmCarroJaTest.java
@@ -0,0 +1,130 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UmCarroJaTest {
+
+    @Test
+    public void registarUtilizador() {
+    }
+
+    @Test
+    public void iniciarSessao() {
+    }
+
+    @Test
+    public void logoutUtilizador() {
+    }
+
+    @Test
+    public void existeUtilizador() {
+    }
+
+    @Test
+    public void existeVeiculo() {
+    }
+
+    @Test
+    public void alugueresClassificarCliente() {
+    }
+
+    @Test
+    public void classificarCliente() {
+    }
+
+    @Test
+    public void registarVeiculo() {
+    }
+
+    @Test
+    public void sinalizarDisponibilidade() {
+    }
+
+    @Test
+    public void abastecerVeiculo() {
+    }
+
+    @Test
+    public void altPrecoKm() {
+    }
+
+    @Test
+    public void determinarListaEspera() {
+    }
+
+    @Test
+    public void respostaProp() {
+    }
+
+    @Test
+    public void determinarListaAlugCli() {
+    }
+
+    @Test
+    public void altPrecoAluguer() {
+    }
+
+    @Test
+    public void totalFactBDates() {
+    }
+
+    @Test
+    public void alugueresClassificarVeiculo() {
+    }
+
+    @Test
+    public void classificarVeiculo() {
+    }
+
+    @Test
+    public void registaAluguer() {
+    }
+
+    @Test
+    public void alugueresEfetuados() {
+    }
+
+    @Test
+    public void maisProximo() {
+    }
+
+    @Test
+    public void maisBarato() {
+    }
+
+    @Test
+    public void maisBaratoNoPerimetro() {
+    }
+
+    @Test
+    public void veiculoEspecifico() {
+    }
+
+    @Test
+    public void determinadaAutonomia() {
+    }
+
+    @Test
+    public void maisPertoJa() {
+    }
+
+    @Test
+    public void maisBaratoJa() {
+    }
+
+    @Test
+    public void alterarPosAutonomiaVeiculo() {
+    }
+
+    @Test
+    public void alterarPosCliente() {
+    }
+
+    @Test
+    public void classificarClienteJa() {
+    }
+
+    @Test
+    public void classificarVeiculoJa() {
+    }
+}
